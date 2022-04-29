6dbb763cb2639fcab2372130d6cfa2db71c49817
concreteguillotine <sethlinex@gmail.com>
2022-04-29 18:03:43 -0400

    added admin manespace with application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    root "application#index"
+  end
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "projects#index"
+    root "projects#index"
 
   resources :projects do
     resources :tickets
