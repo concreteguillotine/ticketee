d5bd1dd0b51d0c5d0359b15254e5737ff79cf07b
concreteguillotine <sethlinex@gmail.com>
2022-05-06 10:04:06 -0400

    added the ability to archive users, includes prevention from self-archiving

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  scope :active, lambda { where(archived_at: nil) }
 
-         def to_s
-          "#{email} (#{admin? ? "Admin" : "User" })"
-         end
+  def to_s
+    "#{email} (#{admin? ? "Admin" : "User" })"
+  end
+
+  def archive!
+    self.update(archived_at: Time.now)
+  end
 end
