5974a5f70e142375464ec2220b5668681091f61b
concreteguillotine <sethlinex@gmail.com>
2022-06-21 16:58:22 -0400

    add the ability for users to remove tags from tickets

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-require("bootstrap");
 import 'jquery'
 import 'popper.js'
 import 'bootstrap'
+require("bootstrap");
+require("../src/tags");
