bc36b0b220d4475d0c3f5d3889ea25f7f8a83261
concreteguillotine <sethlinex@gmail.com>
2022-06-21 08:35:59 -0400

    make tags attachable to existing tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -14,9 +14,7 @@ class TicketsController < ApplicationController
     def create
         @ticket = @project.tickets.build(ticket_params)
         @ticket.author = current_user
-        @ticket.tags = params[:tag_names].split(",").map do |tag|
-            Tag.find_or_initialize_by(name: tag.strip)
-        end
+        @ticket.tags = processed_tags
 
         if @ticket.save
             flash[:notice] = "Ticket has been created."
@@ -32,6 +30,7 @@ class TicketsController < ApplicationController
 
     def update 
         if @ticket.update(ticket_params)
+            @ticket.tags << processed_tags
             flash[:notice] = "Ticket has been updated."
             redirect_to [@project, @ticket]
         else
@@ -60,6 +59,12 @@ class TicketsController < ApplicationController
     end
 
     private
+
+    def processed_tags
+        params[:tag_names].split(",").map do |tag|
+            Tag.find_or_initialize_by(name: tag.strip)
+        end
+    end
     
     def ticket_params
         params.require(:ticket).permit(:name, :description, :attachment)
